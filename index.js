bece5837558fda85080771f20342d7404f8fe3cd
ChunWeiYeah <yher25@gmail.com>
2023-02-20 13:28:11 +0800

    update action

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const axios = require("axios");
+
+const getAPIData = async () => {
+  const data = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  console.log(data);
+};
 
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("sitemap");
   console.log(`Hello ${nameToGreet}!`);
+  console.log(getAPIData);
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
