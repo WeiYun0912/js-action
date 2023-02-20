3df6d0038d5a133245e4402a3bd3353753e5bca6
ChunWeiYeah <yher25@gmail.com>
2023-02-20 16:35:01 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # js-action
+
+## Hello World
+
+<!-- UPDATE_WEISITE:START -->
+
+<!-- UPDATE_WEISITE:END -->
--- index.js
+++ index.js
@@ -2,24 +2,82 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const { Toolkit } = require("actions-toolkit");
 const fs = require("fs");
-// try {
-//   // `who-to-greet` input defined in action metadata file
-//   const nameToGreet = core.getInput("sitemap");
-//   console.log(`Hello ${nameToGreet}!`);
-//   console.log(getAPIData());
-//   const time = new Date().toTimeString();
-//   core.setOutput("time", time);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2);
-//   console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+const MAX_LINES = core.getInput("MAX_LINES");
+
+const baseUrl = "https://weiyun0912.github.io";
+
+async function getBlogOutline() {
+  const { data } = await axios.get(
+    "https://weiyun0912.github.io/Wei-Docusaurus/docs/intro"
+  );
+
+  const $ = cheerio.load(data);
+
+  const outline = [];
+
+  const Logs = $("h1:contains('Logs')").next().children();
+
+  Logs.each((_, el) => {
+    const logDetail = {
+      title: "",
+      link: "",
+    };
+
+    logDetail.title = $(el).text();
+    logDetail.link = baseUrl + $(el).children().attr("href");
+
+    outline.push(logDetail);
+  });
+
+  const outlineFilter = outline.slice(0, "5");
+  console.log(outlineFilter);
+  return outlineFilter;
+}
 
 Toolkit.run(async (tools) => {
   tools.log.debug("Edit README.md Start...");
 
   const readmeContent = fs.readFileSync("./README.md", "utf-8").split("\n");
-  console.log(readmeContent);
+
+  //find update comments
+  let startIndex = readmeContent.findIndex(
+    (content) => content.trim() === "<!-- UPDATE_WEISITE:START -->"
+  );
+
+  // not exists
+  if (startIndex === -1)
+    return tools.exit.failure("Not Found Start Update Comments");
+
+  let endIndex = readmeContent.findIndex(
+    (content) => content.trim() === "<!-- UPDATE_WEISITE:END -->"
+  );
+
+  if (endIndex === -1)
+    return tools.exit.failure("Not Found End Update Comments");
+
+  const outline = await getBlogOutline();
+  if (startIndex !== -1 && endIndex !== -1) {
+    startIndex++; //next line
+
+    outline.forEach((o, index) => {
+      readmeContent.splice(
+        startIndex + index,
+        0,
+        `- ${o.title} [連結](${o.link})`
+      );
+    });
+
+    // readmeContent.splice(
+    //   startIndex + activitys.length,
+    //   0,
+    //   "<!-- UPDATE_WEISITE:END -->"
+    // );
+
+    fs.writeFileSync("./README.md", readmeContent.join("\n"));
+  }
+
   tools.exit.success("down.");
 });
